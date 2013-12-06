e39bf4e0a7234848597cd2cff09ae45ac4d1fc38
Eshwar <eshwar@eshwar-Inspiron-1440.(none)>
2013-12-06 23:24:28 +0530

    change model file neme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppGit::Application.config.secret_key_base = 'd21cb0d5e41f0a01c0bbd0ba296249b36b31de51993d8ab997b1e4f97b2875a420b8c102da3ae59905c73e6030a7585538240fac742e55137b3b710b22acf26e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppGit::Application.config.session_store :cookie_store, key: '_app_git_session'
